c74f97b0fc22a001d3b1c5d24d921d624ba4f76a
rohityadav <rohityadavrv19@gmail.com>
2021-03-12 00:12:45 +0530

    firstcommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
